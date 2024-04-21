6f8d2265f99fd29d0571c72d1c6d266323306b7f
mussa-chitemo <mussachitemo6@gmail.com>
2024-04-21 15:16:11 +0300

    shoes website

--- scrip.js
+++ scrip.js
@@ -6,3 +6,14 @@ hambager.addEventListener('click', ()=>{
      hambager.classList.toggle('active');
      navs.classList.toggle('active');
 });
+
+// for button 
+const buy = document.querySelector('.buy-now');
+const explore = document.querySelector('.explore');
+
+buy.addEventListener('click', ()=>{
+     alert('The system is under maintanance. Right!!')
+})
+explore.addEventListener('click', ()=>{
+     alert('The system is under maintanance. Right!! please wait, update very soon')
+})
